575dba15a2a76f28a858cf51d770e2509b491831
niallliddane <niallliddane@outlook.ie>
2025-11-06 17:05:42 +0000

    LoginTest push

--- src/Java/LoginTest.java
+++ src/Java/LoginTest.java
@@ -11,7 +11,7 @@ public class LoginTest {
         int userID = userInput.nextInt();
         userInput.nextLine();
 
-        System.out.print("Password 300 ");
+        System.out.print("Password 200 ");
         String password = userInput.nextLine();
 
         lm.setPassword(userID, password);
