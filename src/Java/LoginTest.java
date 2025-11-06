4522c4d4bec61f6de4da41bddae725ba443b9478
Eoin Power <24438081@studentmail.ul.ie>
2025-11-06 16:45:52 +0000

    LoginTest push

--- src/Java/LoginTest.java
+++ src/Java/LoginTest.java
@@ -11,7 +11,7 @@ public class LoginTest {
         int userID = userInput.nextInt();
         userInput.nextLine();
 
-        System.out.print("Password: ");
+        System.out.print("Pass: ");
         String password = userInput.nextLine();
 
         lm.setPassword(userID, password);
