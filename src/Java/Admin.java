5a3ed2dc2520f5397423c6f276bfe3026084248e
Eoin Power <24438081@studentmail.ul.ie>
2025-11-06 20:09:46 +0000

    fixed file structure

--- src/Java/Admin.java
+++
@@ -1,6 +0,0 @@
-public class Admin extends User {
-
-    public Admin(int id, String name, String email) {
-        super(id, name, email);
-    }
-}
--- src/Java/Lecturer.java
+++
@@ -1,6 +0,0 @@
-public class Lecturer extends User {
-
-    public Lecturer(int id, String name, String email) {
-        super(id, name, email);
-    }
-}
