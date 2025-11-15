f8ee005c4fef5873135d3c2be1fc6f71b131ea46
niallliddane <niallliddane@outlook.ie>
2025-11-15 20:53:47 +0000

    Teaching Space: Lecture, lab, tutorial

--- src/TeachingSpace.java
+++ src/TeachingSpace.java
@@ -17,10 +17,14 @@ public abstract class TeachingSpace {
         this.capacity = capacity;
         this.RoomType = RoomType;
     }
-    public final String getId() {return id;}
-    public final String getName() {return name;}
-    public final int getCapacity() {return capacity;}
-    public final String getType() {return RoomType;}
+    public final String getId() {
+        return id;}
+    public final String getName() {
+        return name;}
+    public final int getCapacity() {
+        return capacity;}
+    public final String getType() {
+        return RoomType;}
 
     public Boolean isValidType(String RoomType) {
         for (int i = 0; i < ValidTypes.length; i++) {
