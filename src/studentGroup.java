4c109f6556cc874d55abbbde4433f3fb6819bdc2
TaraKingston <tara.kingston05@gmail.com>
2025-11-18 20:47:26 +0000

    JavaDocs comments added

--- src/studentGroup.java
+++ src/studentGroup.java
@@ -1,6 +1,6 @@
 /**
  * this class represents a group of students enrolled in a module and programme
- * student groups are formed from the students enrolled in a programme and year
+ * student groups are formed from the students enrolled in a programme and year.
  * this data is read from a csv file of all users
  * in this way rooms and modules can be scheduled for an entire studentGroup rather than per student.
  * student groups are split into sub-groups for labs and tutorials.
