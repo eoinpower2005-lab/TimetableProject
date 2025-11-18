7fff309069b56ac853a889edf4dddfa70f93467f
TaraKingston <tara.kingston05@gmail.com>
2025-11-18 13:23:47 +0000

    JavaDocs comments added

--- src/Room.java
+++ src/Room.java
@@ -4,4 +4,13 @@
  * a room has attributes id, type, capacity and availability.
  */
 public class Room {
+    private String roomName;
+    private String roomType;
+    private int roomCapacity;
+
+public Room(String roomName, String roomType, int roomCapacity){
+    this.roomName = roomName;
+    this.roomType = roomType;
+    this.roomCapacity = roomCapacity;
+  }
 }
