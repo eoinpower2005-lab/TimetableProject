451b38ef9e134ddb6c121e99b113de6426d5c1ab
niallliddane <niallliddane@outlook.ie>
2025-11-29 23:04:05 +0000

    Rooms and students

--- src/StudentGroup.java
+++ src/StudentGroup.java
@@ -1,10 +1,10 @@
 /**
- * represents a group of students enrolled in a module and programme
+ * Represents a group of students enrolled in a module and programme
  * student groups are formed from the students enrolled in a programme and year.
- * this data is read from a csv file of all users
- * in this way rooms and modules can be scheduled for an entire studentGroup rather than per student.
- * student groups are split into sub-groups for labs and tutorials.
- * the capacity of a room is compared against the size of the sub-groups.
+ * This data is read from a csv file of all users
+ * In this way rooms and modules can be scheduled for an entire studentGroup rather than per student.
+ * Student groups are split into sub-groups for labs and tutorials.
+ * The capacity of a room is compared against the size of the sub-groups.
  */
 public class StudentGroup {
     private String groupID; //ID for the group so it can be identified "LM124H1S1_CS4141_Lab1"
