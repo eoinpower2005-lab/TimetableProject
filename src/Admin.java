33eb8432f65baadb89030e824fc2ede8a43ec65d
niallliddane <niallliddane@outlook.ie>
2025-12-01 08:44:14 +0000

    admin

--- src/Admin.java
+++ src/Admin.java
@@ -2,28 +2,28 @@
  * Admin represents the unique attributes and behaviours of an admin object
  * Admin is a subclass of the User object
  */
-public class Admin extends User {
+public class Admin extends User { //
 
     /**
-     * admin constructor which stores the unique attributes of an admin
-     * calls the superclass User constructor using the super keyword
-     * @param userid the unique id of each user
-     * @param password the unique password of each user
-     * @param name the name of each user
-     * @param email the unique email of each user
-     * @param role the role of an admin
+     * admin constructor which stores the unique attributes of an admin /
+     * calls the superclass User constructor using the super keyword /
+     * @param userids the unique id of each user /
+     * @param passwords the unique password of each user /
+     * @param names the name of each user /
+     * @param emails the unique email of each user /
+     * @param roles the role of an admin /
      */
-    public Admin(int userid, String password, String name, String email, String role, String timetableID) {
-        super(userid, password, name, email, role, timetableID);
+    public Admin(int userids, String passwords, String names, String emails, String roles, String timetableIDs) {
+        super(userids, passwords, names, emails, roles, timetableIDs);
     }
 
     /**
-     * overrides the abstract method in the superclass User object
-     * admin can modify a timetable, hence true
-     * @return returns true
+     * overrides the abstract method in the superclass User object /
+     * admin can modify a timetable, hence true /
+     * @return returns true /
      */
     @Override
-    public boolean canModifyTimetable() {
-        return true;
+    public boolean canModifyTimetable() { //
+        return true; //
     }
 }
