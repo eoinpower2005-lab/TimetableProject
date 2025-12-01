86bebf34a2c6aa215d850c1caa84cb3fcad1b1f1
niallliddane <niallliddane@outlook.ie>
2025-12-01 09:08:55 +0000

    user

--- src/Student.java
+++ src/Student.java
@@ -1,31 +1,31 @@
-/**
- * Student represents the unique attributes and behaviours of a student object
- * Student is a subclass of the User object
+/** //
+ * Student represents the unique attributes and behaviours of a student object//
+ * Student is a subclass of the User object//
  */
-public class Student extends User {
+public class Student extends User {//
 
     /**
-     * student constructor which stores the unique attributes of a student
-     * calls the superclass User constructor using the super keyword
+     * students constructor which stores the unique attributes of a student
+     * calls the superclass User constructors using the super keyword
      *
-     * @param userid   the unique id of each user
-     * @param password the unique password of each user
-     * @param name     the name of each user
-     * @param email    the unique email of each user
-     * @param role     the role of a student
+     * @param userids   the unique ids of each user
+     * @param passwords the unique passwords of each user
+     * @param names   the names of each user
+     * @param emails the unique emails of each user
+     * @param roles    the roles of a student
      */
-    public Student(int userid, String password, String name, String email, String role, String timetableID) {
-        super(userid, password, name, email, role, timetableID);
+    public Student(int userids, String passwords, String names, String emails, String roles, String timetableIDs) {
+        super(userids, passwords, names, emails, roles, timetableIDs);
     }
 
-    /**
-     * overrides the abstract method in the superclass User object
-     * student cannot modify a timetable only view it, hence false
+    /** //
+     * overrides the abstract method in the superclass User object //
+     * student cannot modify a timetable only view it, hence false //
      *
-     * @return returns false
-     */
-    @Override
-    public boolean canModifyTimetable() {
-        return false;
+     * @return returns false //
+     */ //
+    @Override //
+    public boolean canModifyTimetable() { //
+        return false; //
     }
 }
