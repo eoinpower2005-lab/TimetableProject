38761d2c01ca8980bfd47bd19257647185454865
TaraKingston <tara.kingston05@gmail.com>
2025-12-01 14:33:00 +0000

    Update classes and java docs

--- src/ModuleContactHours.java
+++ src/ModuleContactHours.java
@@ -24,4 +24,86 @@ public class ModuleContactHours {
      * @param labHours the weekly lab hours required for a module
      */
 
+    public ModuleContactHours(String moduleCode, int lectureHours, int tutorialHours, int labHours) {
+        this.moduleCode = moduleCode;
+        this.lectureHours = lectureHours;
+        this.tutorialHours = tutorialHours;
+        this.labHours = labHours;
+    }
+
+    /**
+     *
+     * @return the module code as a String identifying the module e.g. "CS4013"
+     */
+
+    public String getModuleCode() {
+         return moduleCode;
+    }
+
+    /**
+     * Sets the moduleCode
+     * @param moduleCode the new module code for this contract hours recorded
+     */
+
+    public void setModuleCode(String moduleCode) {
+         this.moduleCode = moduleCode;
+    }
+
+    /**
+     *
+     * @return the number of lecture hours as positive Integers required for the module.
+     * Lecture hours represent large group teaching sessions delivered in halls or classrooms
+     */
+
+    public int getLectureHours() {
+         return lectureHours;
+    }
+
+    /**
+     * Sets the weekly lecture hours
+     * @param lectureHours the new number of lecture hours as a positive Integer required for the module
+     */
+
+    public void setLectureHours(int lectureHours) {
+         this.lectureHours = lectureHours;
+    }
+
+    /**
+     *
+     * @return the number of tutorial hours as a positive integer required for the module
+     * the tutorial hours represent a small group teaching sessions held in classes
+     */
+
+    public int getTutorialHours() {
+        return tutorialHours;
+    }
+
+    /**
+     * Sets the weekly tutorial hours
+     * @param tutorialHours the new number of weekly tutorial hours required for the module
+     */
+
+    public void setTutorialHours(int tutorialHours) {
+         this.tutorialHours = tutorialHours;
+    }
+
+    /**
+     *
+     * @return the number of weekly lab hours required for a module
+     */
+
+    public int getLabHours() {
+         return labHours;
+    }
+
+    /**
+     * Sets the weekly lab hours
+     * @param labHours the new number of weekly lab hours required for the module
+     */
+
+    public void setLabHours(int labHours) {
+         this.labHours = labHours;
+    }
+}
+
 
