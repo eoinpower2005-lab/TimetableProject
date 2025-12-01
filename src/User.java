39c26523cc722644560f5b31f9aa3cff2213e657
niallliddane <niallliddane@outlook.ie>
2025-12-01 08:57:22 +0000

    user

--- src/User.java
+++ src/User.java
@@ -3,12 +3,12 @@
  * stores commonly shared attributes userid, password, name, email, role
  */
 public abstract class User {
-    private int userids;
-    private String passwords;
-    private String names;
-    private String emails;
-    private String roles;
-    private String timetableIDs;
+    private int userid;
+    private String password;
+    private String name;
+    private String email;
+    private String role;
+    private String timetableID;
 
     /**
      *
@@ -19,106 +19,106 @@ public abstract class User {
      * @param role role of user (student, lecturer, admin)
      * constructs a User object with the passed in parameters
      */
-    public User(int userids, String passwords, String names, String emails, String roles, String timetableIDs) {
-        this.userids = userids;
-        this.passwords = passwords;
-        this.names= names;
-        this.emails = emails;
-        this.roles = roles;
-        this.timetableIDs = timetableIDs;
-    }
-
-    /** //
-     * abstract method to be overridden by subclasses to determine if user can modify a timetable //
-     * @return returns true if user can modify timetable, else false //
-     */
-    public abstract boolean canModifyTimetable(); //
-
-    /** //
-     * setter method to set a user id //
-     * @param userid the unique id of each user to be set //
-     */ //
-    public void setId(int userids) {
-        this.userids = userids;
-    }
-
-    /** //
-     * getter method to return the users id //
-     * @return returns userid //
-     */ //
-    public int getId() { //
-        return userids; //
-    }
-
-    /** //
-     * setter method to set a users password //
-     * @param password the unique password for a user to be set //
-     */
-    public void setPassword(String password) { //
-        this.passwords = passwords;
+    public User(int userid, String password, String name, String email, String role, String timetableID) {
+        this.userid = userid;
+        this.password = password;
+        this.name= name;
+        this.email = email;
+        this.role = role;
+        this.timetableID = timetableID;
     }
 
     /**
-     * getter method to return a users password //
-     * @return returns users password //
+     * abstract method to be overridden by subclasses to determine if user can modify a timetable
+     * @return returns true if user can modify timetable, else false
      */
-    public String getPasswords() {
-        return passwords;
+    public abstract boolean canModifyTimetable();
+
+    /**
+     * setter method to set a user id
+     * @param userid the unique id of each user to be set
+     */
+    public void setId(int userid) {
+        this.userid = userid;
     }
 
     /**
-     * setter method to set the name of a user //
-     * @param names users name //
+     * getter method to return the users id
+     * @return returns userid
      */
-    public void setName(String names) {
-        this.names = names;
+    public int getId() {
+        return userid;
+    }
+
+    /** //
+     * setter method to set a users password
+     * @param password the unique password for a user to be set
+     */
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     /**
-     * getter method to return a users name //
-     * @return returns users name //
+     * getter method to return a users password
+     * @return returns users password
      */
-    public String getNames() {
-        return names;
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * setter method to set the name of a user
+     * @param name users name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * getter method to return a users name
+     * @return returns users name
+     */
+    public String getName() {
+        return name;
     }
 
     /**
      * setter method to set the email of a user
-     * @param emails users email
+     * @param email users email
      */
-    public void setEmails(String email) {
-        this.emails = emails;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     /**
-     * getter method to return the users email //
-     * @return returns users email //
+     * getter method to return the users email
+     * @return returns users email
      */
-    public String getEmails() {
-        return emails;
+    public String getEmail() {
+        return email;
     }
 
     /**
-     * setter method to set a users role //
-     * @param roles users role //
+     * setter method to set a users role
+     * @param role users role
      */
-    public void setRole(String roles) {
-        this.roles = roles;
+    public void setRole(String role) {
+        this.role = role;
     }
 
     /**
      * getter method to return the users role
      * @return returns users role
      */
-    public String getRoles() {
-        return roles;
+    public String getRole() {
+        return role;
     }
 
-    public void setTimetableIDs(String timetableIDs) {
-        this.timetableIDs = timetableIDs;
+    public void setTimetableID(String timetableID) {
+        this.timetableID = timetableID;
     }
 
-    public String getTimetableIDs() {
-        return timetableIDs;
+    public String getTimetableID() {
+        return timetableID;
     }
 }
