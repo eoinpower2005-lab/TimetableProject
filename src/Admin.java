b5d767959049432107f8ab13dc7866925b3e73a1
niallliddane <niallliddane@outlook.ie>
2025-12-01 08:47:38 +0000

    admin

--- src/Admin.java
+++ src/Admin.java
@@ -1,10 +1,10 @@
-/**
- * Admin represents the unique attributes and behaviours of an admin object
- * Admin is a subclass of the User object
+/** //
+ * Admin represents the unique attributes and behaviours of an admin object //
+ * Admin is a subclass of the User object //
  */
 public class Admin extends User {
 
-    /**
+    /** //
      * admin constructor which stores the unique attributes of an admin /
      * calls the superclass User constructor using the super keyword /
      * @param userid the unique id of each user /
@@ -15,15 +15,15 @@ public class Admin extends User {
      */
     public Admin(int userid, String password, String name, String email, String role, String timetableID) {
         super(userid, password, name, email, role, timetableID);
-    }
-
-    /**
+    } //
+//
+    /** //
      * overrides the abstract method in the superclass User object
      * admin can modify a timetable, hence true
      * @return returns true
-     */
-    @Override
+     */ //
+    @Override //
     public boolean canModifyTimetable() {
         return true;
-    }
-}
+    } //
+} //
