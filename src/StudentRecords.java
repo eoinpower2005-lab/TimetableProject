bb7a0e17307a16dbf612ff158f33cf9f0d872f56
niallliddane <niallliddane@outlook.ie>
2025-12-01 09:26:15 +0000

    studentrec

--- src/StudentRecords.java
+++ src/StudentRecords.java
@@ -1,37 +1,37 @@
-public class StudentRecords { //
+public class StudentRecords {
     private String ModuleCode;
     private int NumStudents;
     /**
      * @param moduleCode the code of the module
      * @param numStudents the amount of students enrolled in this module
      */
-    public StudentRecords(String moduleCode, int numStudents) { //
+    public StudentRecords(String moduleCode, int numStudents) {
         this.ModuleCode = moduleCode;
         this.NumStudents = numStudents;
-    }//
+    }
     /**
      * @return the codes module
      */
-    public String getModuleCode() { //
+    public String getModuleCode() {
         return ModuleCode;
-    } //
+    }
     /**
      *
      * @return the number of students
      */
     public int getNumStudents() {
         return NumStudents;
-    } //
+    }
     /**
      * @param numStudents sets a new student count
      */
     public void setNumStudents(int numStudents) {
         this.NumStudents = numStudents;
-    } //
+    }
     /**
      * @param ModuleCode sets a new module code
      */
     public void setModuleCode(String ModuleCode) {
         this.ModuleCode = ModuleCode;
-    } //
-} //
+    }
+}
