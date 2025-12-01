a00987a56f425c631d3d00f5305a0078f988d475
niallliddane <niallliddane@outlook.ie>
2025-12-01 09:18:05 +0000

    student

--- src/Student.java
+++ src/Student.java
@@ -1,13 +1,13 @@
 /**
  * Student represents the unique attributes and behaviours of a student object
  * Student is a subclass of the User object
- */
+ *///
 public class Student extends User {
-
-    /**
+//
+    /** //
      * students constructor which stores the unique attributes of a student
      * calls the superclass User constructors using the super keyword
-     *
+     * //
      * @param userid   the unique id of each user
      * @param password the unique password of each user
      * @param name   the name of each user
@@ -21,7 +21,7 @@ public class Student extends User {
     /**
      * overrides the abstract method in the superclass User object
      * student cannot modify a timetable only view it, hence false
-     *
+     * //
      * @return returns false
      */
     @Override
